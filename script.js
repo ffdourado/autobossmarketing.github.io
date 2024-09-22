0955d8c07b5e99d3eb49ea5da8a3a9100c5bf508
ffdourado <161772373+ffdourado@users.noreply.github.com>
2024-09-22 01:23:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -17,6 +17,9 @@ const observer = new IntersectionObserver(entries => {
             entry.target.classList.add('in-view');
         }
     });
+}, { threshold: 0.1 }); // 10% of the element must be visible to trigger
+
+    });
 });
 
 animatedElements.forEach(element => {
