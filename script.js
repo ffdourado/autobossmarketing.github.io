e6ab1c3e4c9de633e820b562d236fb945eb34659
ffdourado <161772373+ffdourado@users.noreply.github.com>
2024-09-22 00:39:33 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-
-document.addEventListener("scroll", function() {
-    const parallax = document.querySelectorAll(".parallax");
-    parallax.forEach(function(el) {
-        let speed = el.getAttribute("data-speed");
-        el.style.transform = `translateY(${window.scrollY * speed}px)`;
-    });
-});
